5d39d70741a351f22cca8a2b012148f01ef8bd73
SKYLAKE1314 <143381227+SKYLAKE1314@users.noreply.github.com>
2026-08-28 13:52:29 +0800

    feat: 精簡二分類測試類，風格保持與 WorldTest 和 textdetection 一致

---
+++ Detection/ClassifyTest.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+# 載入訓練好的二分類模型權重檔 (.pt)
+model = YOLO(r"Z:\VisionTek\Ultralytics\Ultralytics_YOLO\runs\classify\train-5\weights\best.pt")
+
+# 進行推論
+results = model.predict(r"Z:\VisionTek\Ultralytics\Ultralytics_YOLO\NYA_Project\dataset\val\OK\工位1_工位1_26-07-20-153712-176_0045_二分类检测_16.jpg", imgsz=512)
+
+# 顯示結果
+results[0].show()
